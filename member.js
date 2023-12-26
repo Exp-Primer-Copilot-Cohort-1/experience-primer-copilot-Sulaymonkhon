b8f94634d32ff32056d3fcd6fba949c935b9be09
peace <96327651+Sulaymonkhon@users.noreply.github.com>
2023-12-26 07:10:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,23 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var memberName = document.getElementById("memberName");
+    var memberEmail = document.getElementById("memberEmail");
+    var memberPhone = document.getElementById("memberPhone");
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsList = document.getElementById("memberSkillsList");
+    var memberSkillsListItems = document.getElementById("memberSkillsListItems");
+    var memberSkillsListItemsList = document.getElementById("memberSkillsListItemsList");
+    var memberSkillsListItemsListItems = document.getElementById("memberSkillsListItemsListItems");
+    var memberSkillsListItemsListItemsList = document.getElementById("memberSkillsListItemsListItemsList");
+    var memberSkillsListItemsListItemsListItems = document.getElementById("memberSkillsListItemsListItemsListItems");
+    var memberSkillsListItemsListItemsListItemsList = document.getElementById("memberSkillsListItemsListItemsListItemsList");
+    var memberSkillsListItemsListItemsListItemsListItems = document.getElementById("memberSkillsListItemsListItemsListItemsListItems");
+    var memberSkillsListItemsListItemsListItemsListItemsList = document.getElementById("memberSkillsListItemsListItemsListItemsListItemsList");
+    var memberSkillsListItemsListItemsListItemsListItemsListItems = document.getElementById("memberSkillsListItemsListItemsListItemsListItemsListItems");
+    var memberSkillsListItemsListItemsListItemsListItemsListItemsList = document.getElementById("memberSkillsListItemsListItemsListItemsListItemsListItemsList");
+    var memberSkillsListItemsListItemsListItemsListItemsListItemsListItems = document.getElementById("memberSkillsListItemsListItemsListItemsListItemsListItemsListItems");
+    var memberSkillsListItemsListItemsListItemsListItemsListItemsListItemsList = document.getElementById("memberSkillsListItemsListItemsListItemsListItemsListItemsListItemsList");
+    var memberSkillsListItemsListItemsListItemsListItemsListItemsListItemsListItems = document.getElementById("memberSkillsListItemsListItemsListItemsListItemsListItemsListItemsListItems");
+    var memberSkillsListItemsListItemsListItemsListItemsListItemsListItemsListItemsList = document.getElementById("memberSkillsListItemsListItemsListItemsListItemsListItemsListItemsListItemsList");
+}    
